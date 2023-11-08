7832adb3d387225e381ad811a24c81d5c1e7c86f
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-08 12:56:20 +0100

    Reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [userInput, setUserInput] = useState("")
-  const [num1] = useState(5)
-  const [num2] = useState(5)
+  const [userInput, setUserInput] = useState("");
+  const [num1] = useState(5);
+  const [num2] = useState(5);
 
-  const sum = () => num1 + num2
+  const sum = () => num1 + num2;
   useEffect(() => {
-    console.log(`New sum. Value: ${sum()}`)
-  }, [sum])
+    console.log(`New sum. Value: ${sum()}`);
+  }, [sum]);
   return (
     <main className="App">
-        <input type="text" placeholder="input" 
-        value={userInput} onChange={(e) => setUserInput(e.target.value)} 
-        />
-        <h1>outPut: {userInput || ".."}</h1>
+      <input
+        type="text"
+        placeholder="input"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <h1>outPut: {userInput || ".."}</h1>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
