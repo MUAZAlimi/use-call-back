0c5c7cd55701eed581479a6242d9ca065fed7d46
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-08 12:17:10 +0100

    terget

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <main className="App">
         <input type="text" placeholder="input"
-        value={userInput} onChange={(e) => setUserInput()} 
+        value={userInput} onChange={(e) => setUserInput(e.target.value)} 
         />
     </main>
   )
